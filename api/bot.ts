c1b9db4aa4c6d7ff331813e27eda39c18878a2a3
Inbestigator <inbestigatoryt@gmail.com>
2025-10-25 18:28:46 -0700

    ops(hosting): remove vercel deployment

--- api/bot.ts
+++
@@ -1,4 +0,0 @@
-import { handleRequest } from "dressed/server";
-import { commands, components, config, events } from "../.dressed/index.js";
-
-export const POST = (req: Request) => handleRequest(req, commands, components, events, config);
